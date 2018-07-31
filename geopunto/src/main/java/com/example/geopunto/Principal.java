8129b50305154be377dad1bfb87ac39d4303134d
Your Name <you@example.com>
2018-07-31 20:49:41 +0200

    initial commit

---
+++ geopunto/src/main/java/com/example/geopunto/Principal.java
@@ -0,0 +1,11 @@
+package com.example.geopunto;
+
+public class Principal {
+    public static void main(String[] main) {
+        GeoPunto x, y;
+        x = new GeoPunto(-1.5, 3.0);
+        y = new GeoPunto(-1.2, 2.4);
+        x.distancia(y);
+        System.out.println("Complejo: " + x.toString());
+    }
+}
